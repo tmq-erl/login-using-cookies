c125aee9ae453697078545b7c98b6ea034c9fb21
ErlJocson <jocsonerl@gmail.com>
2022-12-22 20:09:06 +0800

    INITIAL COMMIT

---
+++ facebook_with_cookies.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs").promises;
+const { runReuseCookie } = require("../facebook_with_cookies_test");
+
+require("dotenv").config();
+
+const run = async () => {
+  console.log("Logging in");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://www.facebook.com/login");
+  await page.type("#email", process.env.EMAIL);
+  await page.type("#pass", process.env.PASSWORD);
+
+  await page.click("#loginbutton");
+
+  const toSaveCookies = await page.cookies();
+  await fs.writeFile("./cookies.json", JSON.stringify(toSaveCookies, null, 2));
+  await page.waitForNavigation();
+
+  // screenshot after login
+  await page.screenshot({ path: "after-login.jpg" });
+
+  console.log("logged in");
+
+  console.log("Logging in using cookies");
+
+  const cookies = require("../cookies.json");
+  const context = await browser.createIncognitoBrowserContext();
+  const page2 = await context.newPage();
+  await page2.setCookie(...cookies);
+  await page2.goto("https://www.facebook.com/");
+
+  // screen shot after login using cookies
+  await page2.screenshot({ path: "login-using-cookies.jpg" });
+  await browser.close();
+
+  console.log("Logged in using cookies");
+};
+
+run();
